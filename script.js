3e8f54459abcf796bef9065df085c33574264961
FerSancho23 <fersanchez230624@gmail.com>
2025-09-14 17:46:41 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+function mostrarCarta() {
+  const carta = document.getElementById("carta");
+  carta.style.display = carta.style.display === "none" ? "block" : "none";
+}
